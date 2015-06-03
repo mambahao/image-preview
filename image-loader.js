143ccacfbc2c28e953e8fa7d72f95290413f2722
Justin Hao <justin@haoyufeng.com>
2015-06-04 00:27:12 +0800

    change callback run time

--- image-loader.js
+++ image-loader.js
@@ -4,97 +4,93 @@
  * Copyright (c) Aleksandar Gosevski
  */
 
-;(function () {
-    var defaults = function (defaults, options) {
-        for (var key in options) {
-            if (options.hasOwnProperty(key)) {
-                defaults[key] = options[key];
-            }
-        }
+;
+(function () {
+  var defaults = function (defaults, options) {
+    for (var key in options) {
+      if (options.hasOwnProperty(key))
+        defaults[key] = options[key];
+    }
 
-        return defaults;
-    };
+    return defaults;
+  };
 
-    var ImageLoader = function (element, options) {
-        if (!this.isSupported) {
-            throw new Error('Older browsers are not supported.');
-        }
+  var ImageLoader = function (element, options) {
+    if (!this.isSupported)
+      throw new Error('Older browsers are not supported.');
 
-        var opts = typeof options !== 'undefined' ? options : {};
+    var opts = typeof options !== 'undefined' ? options : {};
 
-        if (typeof opts !== 'object' || Array.isArray(opts)) {
-            throw new Error('Options must be passed as object!');
-        }
+    if (typeof opts !== 'object' || Array.isArray(opts))
+      throw new Error('Options must be passed as object!');
 
-        this.setElements(element, opts);
-        this.bindEvents();
-    };
+    this.setElements(element, opts);
+    this.bindEvents();
+  };
 
-    ImageLoader.prototype = {
-        defaults: {
-            placeholder: '.image-placeholder',
-            width: 'auto',
-            height: 'auto',
-            imgClass: 'img-load',
-            method: 'html',
-            callback: null
-        },
+  ImageLoader.prototype = {
+    defaults: {
+      placeholder: '.image-placeholder',
+      width: 'auto',
+      height: 'auto',
+      imgClass: 'img-load',
+      method: 'html',
+      callback: null
+    },
 
-        isSupported: function () {
-            return window.File && window.FileReader && window.FileList && window.Blob;
-        },
+    isSupported: function () {
+      return window.File && window.FileReader && window.FileList && window.Blob;
+    },
 
-        setElements: function (element, options) {
-            this.options = defaults(this.defaults, options);
-            this.$element = document.querySelectorAll(element)[0];
-            this.$placeholder = document.querySelectorAll(this.options.placeholder)[0];
-        },
+    setElements: function (element, options) {
+      this.options = defaults(this.defaults, options);
+      this.$element = document.querySelectorAll(element)[0];
+      this.$placeholder = document.querySelectorAll(this.options.placeholder)[0];
+    },
 
-        bindEvents: function () {
-            this.$element.addEventListener('change', this.render.bind(this), false);
-        },
+    bindEvents: function () {
+      this.$element.addEventListener('change', this.render.bind(this), false);
+    },
 
-        render: function (e) {
-            var uploader = e.currentTarget,
-                reader = new FileReader(),
-                self = this,
-                o = this.options;
+    render: function (e) {
+      var uploader = e.currentTarget,
+          reader = new FileReader(),
+          self = this,
+          o = this.options;
 
-            reader.onload = (function (file) {
-                return self[ o.method === 'css' ? 'setBackground' : 'setImage' ].bind(self);
-            }(uploader.files[0]));
+      reader.onload = (function (file) {
+        return self[o.method === 'css' ? 'setBackground' : 'setImage'].bind(self);
+      }(uploader.files[0]));
 
-            reader.readAsDataURL(uploader.files[0]);
+      reader.readAsDataURL(uploader.files[0]);
+    },
 
-            // If callback is function, execute it
-            if (typeof o.callback === 'function') {
-                o.callback();
-            }
-        },
+    setImage: function (e) {
+      var o = this.options;
 
-        setImage: function (e) {
-            var o = this.options;
+      var image = '<img src="' + e.target.result + '" width="' + o.width + '" height="' + o.height + '" class="' + o.imgClass + '">';
 
-            var image =  '<img src="' + e.target.result + '" width="' + o.width + '" height="' + o.height + '" class="' + o.imgClass + '">';
+      if (o.method === 'html') {
+        this.$placeholder.innerHTML = image;
+        if (typeof o.callback === 'function')
+          o.callback();
+        return;
+      }
 
-            if (o.method === 'html') {
-                this.$placeholder.innerHTML = image;
-                return;
-            }
+      if (o.method === 'append') {
+        this.$placeholder.innerHTML = this.$placeholder.innerHTML + image;
+        if (typeof o.callback === 'function')
+          o.callback();
+        return;
+      }
 
-            if (o.method === 'append') {
-                this.$placeholder.innerHTML = this.$placeholder.innerHTML + image;
-                return;
-            }
+      throw new Error('Only html and append methods are allowed!');
+    },
 
-            throw new Error('Only html and append methods are allowed!');
-        },
+    setBackground: function (e) {
+      this.$placeholder.style.backgroundImage = 'url(' + e.target.result + ')';
+    }
+  };
 
-        setBackground: function (e) {
-            this.$placeholder.style.backgroundImage = 'url(' + e.target.result + ')';
-            return;
-        }
-    };
-
-    window.ImageLoader = ImageLoader;
+  window.ImageLoader = ImageLoader;
 })();
